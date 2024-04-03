bb2af00cd8f2175e8cdf760ca86d2387f99fe321
Michael-Xie <michael.xie@touchless.inc>
2024-04-03 12:11:15 -0400

    feat: add post route and components

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,6 +6,7 @@ import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 import { presentationTool } from "sanity/presentation";
+import { locate } from "@/sanity/presentation/locate";
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import { apiVersion, dataset, projectId } from "./sanity/env";
@@ -23,6 +24,7 @@ export default defineConfig({
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({ defaultApiVersion: apiVersion }),
     presentationTool({
+      locate,
       previewUrl: {
         draftMode: {
           enable: "/api/draft",
